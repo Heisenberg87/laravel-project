3b93126719397b74c119784f0132e44d43f2c1ef
IMPACT\ramizkabul <Ramiz.Kabul@castletonplc.com>
2019-02-08 01:26:43 +0000

    +add bootstrap scss files to project

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
 mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js');
+mix.copy('resources/js/bootstrap.js', 'public/js');
+
