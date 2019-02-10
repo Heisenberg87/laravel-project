4423b8889f43f5fa3c41b032d244bbd3428daca0
IMPACT\ramizkabul <Ramiz.Kabul@castletonplc.com>
2019-02-10 18:55:33 +0000

    + new Footer added to site.

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,24 @@
+<footer id="footer" class="bg-primary">
+    <div class="container">
+        <div class="row text-white">
+            <div class="col-sm-12 text-center py-2">
+
+                <!-- github -->
+                <a class="fb-ic">
+                    <i class="fab fa-github fa-lg text-white mr-md-4 mr-3 fa-2x"></i>
+                </a>
+                <!-- Twitter -->
+                <a class="tw-ic">
+                    <i class="fab fa-twitter fa-lg text-white mr-md-4 mr-3 fa-2x"> </i>
+                </a>
+                <!--Linkedin -->
+                <a class="li-ic">
+                    <i class="fab fa-linkedin fa-lg text-white mr-md-4 mr-3 fa-2x"> </i>
+                </a>
+                <div class="footer-copyright text-center py-2 text-white">&copy; 2018 Copyright:
+                    <a href="https://mdbootstrap.com/education/bootstrap/"> Ramiz Kabul</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
